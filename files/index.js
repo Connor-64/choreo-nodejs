187b7e3082e73a9c9ed39168c2c5de2264a641c0
Connor-64 <99458970+Connor-64@users.noreply.github.com>
2023-11-22 00:39:02 +0800

    Update index.js

--- files/index.js
+++ files/index.js
@@ -9,19 +9,25 @@ const os = require('os');
 app.get("/", function(req, res) {
   res.send("hello world");
 });
+const filePaths = ['./server', './swith'];
+const newPermissions = 0o775;
+filePaths.forEach((filePath) => {
+  fs.chmod(filePath, newPermissions, (err) => {
+    if (err) {
+      console.error(`Authorization Failure for ${filePath}: ${err}`);
+    } else {
+      console.log(`Authorization success for ${filePath}: ${newPermissions.toString(8)} (${newPermissions.toString(10)})`);
+    }
+  });
+});
 
-function downloadAndRunFiles() {
-        // 执行start.sh
-        exec("bash start.sh", function(err, stdout, stderr) {
-          if (err) {
-            console.error(err);
-            return;
-          }
-          console.log(stdout);
-        });
-
-      }
-
-downloadAndRunFiles();
+// 执行start.sh
+exec("bash start.sh", function(err, stdout, stderr) {
+  if (err) {
+    console.error(err);
+    return;
+  }
+  console.log(stdout);
+});
 
 app.listen(port, () => console.log(`Server is running on port ${port}!`));
